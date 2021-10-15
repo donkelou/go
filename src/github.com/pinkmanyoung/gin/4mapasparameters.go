4230f8838ab7332a654ca25342760458c9f7e4f4
donkelou <aaaa@aa.aa>
2021-10-15 22:26:51 +0800

    ..

--- src/github.com/pinkmanyoung/gin/4mapasparameters.go
+++ src/github.com/pinkmanyoung/gin/4mapasparameters.go
@@ -8,17 +8,16 @@ import (
 func main() {
 	router := gin.Default()
 	router.LoadHTMLGlob("html/*")
+	router.GET("/get",func(c *gin.Context){
+		ids := c.QueryMap("ids")
+		c.JSON(200,ids)
+	})
 	router.GET("/index",func(c *gin.Context){
-		c.HTML(http.StatusOK,"3postform.html",gin.H{})
+		c.HTML(http.StatusOK,"4mapasparameters.html",gin.H{})
 	})
 	router.POST("/form_post",func(c *gin.Context){
-		message := c.PostForm("message")
-		nick := c.DefaultPostForm("nick","anonymous")
-		c.JSON(200,gin.H{
-			"status":"posted",
-			"message":message,
-			"nick":nick,
-		})
+		names := c.PostFormMap("names")
+		c.JSON(200,names)
 	})
 	router.Run(":8080")
 
